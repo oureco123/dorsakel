74b754951f87d15fb31a9a636677909ce51ddf82
Majed <majedkutaini@Mac.fritz.box>
2025-08-31 15:39:13 +0200

    Add custom logo and update page title

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,12 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-echo "Deploying to production..."
+echo "Deploying to production from Mac..."
+
+# Commit and push changes to GitHub
 git add .
 git commit -m "$1"
-git push
-ssh dorsakel@92.119.124.79 "cd /home/dorsakel/dorsakel && ./deploy.sh '$1'"
+git push origin main
+
+# SSH into VPS and pull the latest code
+ssh dorsakel@92.119.124.79 "cd /home/dorsakel/dorsakel && ./deploy.sh"
